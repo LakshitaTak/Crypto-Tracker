30eaa5ac04c9d1a623a1189ad7a068a443d1dd11
unknown <lakshitatak33@gmail.com>
2023-12-21 22:18:38 +0530

    initial commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import Header from './components/Common/Header';
+import Footer from './components/Common/Footer';
+
+function App() {
+  return (
+    <div className="App">
+      <Header/>
+      <Footer/>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Common/Header/index.js
@@ -0,0 +1,29 @@
+import React from "react";
+import "./styles.css"
+
+function Header (){
+    return(
+        <div className="navbar">
+            <h1 className="logo">CrytoTracker<span style={{color: "var(--blue)"}}>.</span></h1>
+            <div className="links">
+                <a href="/">
+                <p className="link">Home</p>
+                </a>
+                
+                <a href="/">
+                <p className="link">Compare</p>
+                </a>
+                
+                <a href="/">
+                <p className="link">Watchlist</p>
+                </a>
+                <a href="/">
+                <p className="link">Dashboard</p>
+                </a>
+                
+            </div>
+        </div>
+    )
+}
+
+export default Header

78eb80467f1b63af672eb61288bd450f7bb9b5d2
LakshitaTak <lakshitatak33@gmail.com>
2023-12-23 02:10:19 +0530

    drawer 2buttons added

--- src/components/Common/Header/drawer.js
+++ src/components/Common/Header/drawer.js
@@ -2,17 +2,26 @@ import * as React from 'react';
 import Drawer from '@mui/material/Drawer';
 import MenuRoundedIcon from '@mui/icons-material/MenuRounded';
 import { IconButton } from '@mui/material';
+import { MenuOpen, MenuOpenRounded } from '@mui/icons-material';
 
-export default function TemporaryDrawer() {
+export default function Te0mporaryDrawer() {
     const[open, setOpen] = React.useState(false)
-
+    const[direction , setDirection] = React.useState("left")
+    
+    const handleClick =(dir)=>{
+        setOpen(true)
+        setDirection(dir)
+    }
   return (
     <div>
-          <IconButton onClick={()=>setOpen(true)}>
+          <IconButton onClick={()=>handleClick("right")}>
             <MenuRoundedIcon className='link'/>
           </IconButton>
+          <IconButton onClick={()=>handleClick("left")}>
+            <MenuOpenRounded className='link'/>
+          </IconButton>
           <Drawer
-            anchor={"right"}
+            anchor={direction}
             open={open}
             onClose={()=>setOpen(false)}
           >

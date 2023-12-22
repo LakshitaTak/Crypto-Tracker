7af445fdc13f1af2d64857f944d90bb790b2ee33
LakshitaTak <lakshitatak33@gmail.com>
2023-12-23 01:31:03 +0530

    drawer added

---
+++ src/components/Common/Header/drawer.js
@@ -0,0 +1,40 @@
+import * as React from 'react';
+import Drawer from '@mui/material/Drawer';
+import Button from '@mui/material/Button';
+import MenuRoundedIcon from '@mui/icons-material/MenuRounded';
+import { IconButton } from '@mui/material';
+
+export default function TemporaryDrawer() {
+    const[open, setOpen] = React.useState(false)
+
+  return (
+    <div>
+          <IconButton onClick={()=>setOpen(true)}>
+            <MenuRoundedIcon className='link'/>
+          </IconButton>
+          <Drawer
+            anchor={"right"}
+            open={open}
+            onClose={()=>setOpen(false)}
+          >
+            <div className="drawer-div">
+                <a href="/">
+                <p className="link">Home</p>
+                </a>
+                
+                <a href="/">
+                <p className="link">Compare</p>
+                </a>
+                
+                <a href="/">
+                <p className="link">Watchlist</p>
+                </a>
+                <a href="/">
+                <button className="link">Dashboard</button>
+                </a>
+                
+            </div>
+          </Drawer>
+    </div>
+  );
+}

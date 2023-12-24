8cf2024afed8908d753271732cbdb9fb5bcba057
LakshitaTak <lakshitatak33@gmail.com>
2023-12-25 00:35:07 +0530

    Landing Page left Component updated

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import './App.css';
 import Header from './components/Common/Header';
 import Footer from './components/Common/Footer';
+import MainComponents from './components/LandingPage/MainComponents';
 
 function App() {
   return (
     <div className="App">
       <Header/>
-      <Footer/>
+      {/* <Footer/> */}
+      <MainComponents/>
     </div>
   );
 }

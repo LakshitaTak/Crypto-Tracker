a6669038a126212fbbe3d22d91e9265631a00aba
LakshitaTak <lakshitatak33@gmail.com>
2024-01-08 23:55:03 +0530

    coingecko api integrated

--- src/pages/DashboardPage.js
+++ src/pages/DashboardPage.js
@@ -1,14 +1,28 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import TabsComponent from "../components/Dashboard/Tabs";
 import Header from "../components/Common/Header";
 
-function DashboardPage (){
-    return (
-        <div>
-            <Header />
-            <TabsComponent />
-        </div>
+function DashboardPage() {
+  const [coins, setCoins] = useState([]);
+
+  useEffect(() => {
+    fetch(
+      "https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=100&page=1&sparkline=false&locale=en"
     )
+      .then((response) => response.json())
+      .then((data) => {
+        console.log("data", data);
+        setCoins(data);
+      })
+      .catch((error) => console.log("error", error));
+  });
+
+  return (
+    <div>
+      <Header />
+      <TabsComponent coins={coins}/>
+    </div>
+  );
 }
 
-export default DashboardPage
+export default DashboardPage;
